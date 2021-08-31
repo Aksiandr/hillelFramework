f55f27deebd1b7e875d3f22e790e7c7662ee7667
Aksiandr <Aksiandr@users.noreply.github.com>
2021-08-31 10:28:38 +0300

    hw19

---
+++ src/test/java/TaskBAddToWishListHW19.java
@@ -0,0 +1,24 @@
+import Base.BaseTest;
+import org.openqa.selenium.Alert;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import page.ShopDemoQaHeader;
+import page.ShopDemoQaMain;
+import page.ShopDemoQaWishList;
+
+public class TaskBAddToWishListHW19 extends BaseTest {
+
+    @Test
+    public void addProductToWishList() {
+
+        getWebDriver().get("https://shop.demoqa.com/");
+        ShopDemoQaMain mainPage = new ShopDemoQaMain(getWebDriver());
+        ShopDemoQaHeader header = new ShopDemoQaHeader(getWebDriver());
+        ShopDemoQaWishList wishList = new ShopDemoQaWishList(getWebDriver());
+
+        mainPage.setHovertokioDress();
+        mainPage.setAddTokioToWishList();
+        header.setWishList();
+        Assert.assertTrue(wishList.isProductName("TOKYO TALKIES"));
+    }
+}
